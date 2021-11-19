5c206853f44ee31c1e3366fa6cf00bdabb31f60a
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-11-19 18:25:41 +0500

    contacts

--- src/Screens/Contact.js
+++ src/Screens/Contact.js
@@ -1,5 +1,42 @@
 import React from "react";
+import TableEntryHeadings from "../Components/TableEntryHeadings";
+import TableEntryRows from "../Components/TableEntryRows";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  const tableHeadingRow = [
+    { heading: "Name" },
+    { heading: "Last Name" },
+    { heading: "Email" },
+    { heading: "DOB" },
+    { heading: "Gender" },
+    { heading: "Phone" },
+    { heading: "Message" },
+    { heading: "" },
+  ];
+
+  const tableInfoRow = [
+    { info: "Fahad" },
+    { info: "Ayyaz" },
+    { info: "abc123@gmail.com" },
+    { info: "1-4-2001" },
+    { info: "Male" },
+    { info: "0320-2332134" },
+    { info: "<5 Year" },
+    { placeholder: "Delete", type: "btn_secondary" },
+  ];
+  return (
+    <div className="table__wrapper">
+      <div className="table__wrapper__heading">Contact</div>
+      <TableEntryHeadings tableHeadingEntryRow={tableHeadingRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+      <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+    </div>
+  );
 }
