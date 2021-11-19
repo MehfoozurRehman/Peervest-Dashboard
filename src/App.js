3c2d12750a76a620af320a93ec4f79fc8b772b17
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-19 11:20:52 +0500

    routing done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Dashboard from "./Screens/Dashboard";
+import Login from "./Screens/Login";
+import Investor from "./Screens/Investor";
+import Startup from "./Screens/Startup";
+import StartupDetails from "./Screens/StartupDetails";
+import Client from "./Screens/Client";
+import FAQ from "./Screens/FAQ";
+import Contact from "./Screens/Contact";
+import Milestones from "./Screens/Milestones";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <div className="main">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Main />}>
+            <Route path="" element={<Dashboard />} />
+            <Route path="investor" element={<Investor />} />
+            <Route path="startup" element={<Startup />} />
+            <Route path="startup-details" element={<StartupDetails />} />
+            <Route path="client" element={<Client />} />
+            <Route path="faq" element={<FAQ />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="milestones" element={<Milestones />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
