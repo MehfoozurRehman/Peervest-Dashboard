985cf47b62c84c0fa60407e2d4947df3d54348d4
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-11-20 13:12:31 +0500

    Client Done

--- src/Screens/Client.js
+++ src/Screens/Client.js
@@ -1,5 +1,52 @@
 import React from "react";
+import Button from "../Components/Button";
+import TableEntryHeadings from "../Components/TableEntryHeadings";
+import TableEntryRows from "../Components/TableEntryRows";
 
-export default function Client() {
-  return <div>Client</div>;
+export default function Startup() {
+  const tableHeadingRow = [
+    { heading: "Client Image" },
+    { heading: "Client Name" },
+    { heading: "" },
+    { heading: "" },
+  ];
+
+  const tableInfoRow = [
+    { type: "img" },
+    { info: "IQ Doctor" },
+    {
+      placeholder: "Edit",
+      type: "btn_secondary",
+      path: "/",
+    },
+    {
+      placeholder: "Delete",
+      type: "btn_primary",
+      path: "/",
+    },
+  ];
+  return (
+    <div className="main__container">
+      <div className="main__container__header">
+        <div className="main__container__header__heading">Client</div>
+        <div className="main__container__header__buttons">
+          <Button placeholder="Add Client" className="primary__button" />
+        </div>
+      </div>
+      <div className="main__container__content">
+        <div className="main__container__content__table">
+          <TableEntryHeadings tableHeadingEntryRow={tableHeadingRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+          <TableEntryRows tableEntryInfoRow={tableInfoRow} />
+        </div>
+      </div>
+    </div>
+  );
 }
