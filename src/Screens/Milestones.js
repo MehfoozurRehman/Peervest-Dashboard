6c945e11976f089d94e9f120efd6f1d46b3ead80
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-12-02 00:28:21 +0500

    changes

--- src/Screens/Milestones.js
+++ src/Screens/Milestones.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Button from "../Components/Button";
 import TableEntryHeadings from "../Components/TableEntryHeadings";
 import TableEntryRows from "../Components/TableEntryRows";
 
-export default function Milestones({ setIsEditMileston }) {
+export default function Milestones({ setIsAddMilestone, setIsEditMilestone }) {
   const tableHeadingRow = [
     { heading: "Title" },
     { heading: "Value" },
+    { heading: "" },
     { heading: "" },
   ];
 
@@ -15,14 +17,27 @@ export default function Milestones({ setIsEditMileston }) {
     {
       placeholder: "Edit",
       type: "btn_secondary",
-      onClick: setIsEditMileston,
+      onClick: setIsEditMilestone,
+    },
+    {
+      placeholder: "Delete",
+      type: "btn_primary",
+      onClick: () => {},
     },
   ];
   return (
     <div className="main__container">
       <div className="main__container__header">
         <div className="main__container__header__heading">Milestones</div>
-        <div className="main__container__header__buttons"></div>
+        <div className="main__container__header__buttons">
+          <Button
+            placeholder="Add Milestone"
+            className="primary__button"
+            onClick={() => {
+              setIsAddMilestone(true);
+            }}
+          />
+        </div>
       </div>
       <div className="main__container__content">
         <div className="main__container__content__table">
